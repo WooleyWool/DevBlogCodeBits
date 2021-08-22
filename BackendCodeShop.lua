1182cf486a563121388df86a13be08fee4059ea3
Zayd Abualfellat <zabualfe@umich.edu>
2021-08-22 19:51:22 -0400

    Create BackendCodeShop.lua

---
+++ BackendCodeShop.lua
@@ -0,0 +1,13 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local ShopOptions = {
+	Apples = 10
+}
+
+ReplicatedStorage.BuyItem.OnServerInvoke = function(player, ItemName)
+	local CustomerData = PlayerData[player.Name]
+	
+	if CustomerData["Cash"] >= ShopOptions[ItemName] then
+		CustomerData["Cash"] -= ShopOptions[ItemName] -- Player will have 5 dollars left
+	end
+end)
