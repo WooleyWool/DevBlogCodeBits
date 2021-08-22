adf2943f452650fdecf73f1c0ebabe015f0b6540
Zayd Abualfellat <zabualfe@umich.edu>
2021-08-22 19:04:52 -0400

    Create BackendCode.lua

---
+++ BackendCode.lua
@@ -0,0 +1,10 @@
+local Players = game:GetService("Players")
+
+local PlayerData = {}
+
+Players.PlayerAdded:Connect(function(player)    
+    local PlayerJoinedTable = PlayerData[player.Name] or {}
+    PlayerData[player.Name] = PlayerJoinedData --Creates a new table (for the player's data) within the PlayerData table
+    
+    PlayerJoinedTable["Cash"] = 0
+end)
