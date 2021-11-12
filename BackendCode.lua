f09e38acefc4363be7664944749c0bd9053b16a5
Zayd <zabualfe@umich.edu>
2021-11-12 17:44:16 -0500

    Update BackendCode.lua

--- BackendCode.lua
+++ BackendCode.lua
@@ -7,4 +7,4 @@ Players.PlayerAdded:Connect(function(player)
     PlayerData[player.Name] = PlayerJoinedData --Creates a new table (for the player's data) within the PlayerData table
     
     PlayerJoinedTable["Cash"] = 0
-end)
+end
